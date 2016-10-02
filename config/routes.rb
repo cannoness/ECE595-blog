dc91e89449b5a43ef57a4552671165cba7234e66
abbyjacoby <abby.jacoby@gmail.com>
2016-10-02 16:54:18 -0600

    Added rolify, validations, user and model relationships.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'posts#index'
   resources :comments
-  resources :posts
+  resources :posts, :has_many => :comments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
