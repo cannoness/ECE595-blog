88a1e550cabf9affce804e1623ed2744e79d1058
abbyjacoby <abby.jacoby@gmail.com>
2016-10-04 15:02:28 -0600

    added default user role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
+  after_create :assign_default_role
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   validates_presence_of :email
+
+  def assign_default_role
+     self.add_role(:user) if self.roles.blank?
+  end
+    
 end
